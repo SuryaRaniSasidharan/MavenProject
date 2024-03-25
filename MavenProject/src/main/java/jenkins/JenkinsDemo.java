ce75dc774300345a444e753411e1971cacfa83e6
lenovo <lenovo@LAPTOP-S8UE8KBK>
2024-03-25 22:19:29 +0530

    First commit

---
+++ MavenProject/src/main/java/jenkins/JenkinsDemo.java
@@ -0,0 +1,17 @@
+package jenkins;
+
+public class JenkinsDemo {
+	
+	public void display()
+	{
+		System.out.println("Jenkins Created");
+	}
+
+	public static void main(String[] args) {
+		
+		JenkinsDemo obj=new JenkinsDemo();
+		obj.display();
+
+	}
+
+}
